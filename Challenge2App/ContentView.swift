d7b3e61e152ded35c2a1caea14c4ad2d59ae88ff
TT <tiffanytongxy@gmail.com>
2025-08-16 12:00:43 +0800

    Coded the -ContentView -BuffetAreaView -ContentView -LearnView -LoungeView -MCQView -NormalRoomView -SuiteView

--- Challenge2App/ContentView.swift
+++ Challenge2App/ContentView.swift
@@ -9,11 +9,211 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            Color(Color(red: 0.8, green: 0.2, blue: 0.4, opacity: 1.0))
+        NavigationStack {
+            ZStack {
+                Color(red: 255/255, green: 248/255, blue: 232/255)
+                    .ignoresSafeArea()
+                
+                VStack {
+                    Spacer()
+                    Image("Hotel")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 400, height: 400)
+                    Spacer()
+                }
+                HStack {
+                    Spacer()
+                    Spacer()
+                    VStack {
+                        Spacer()
+                        NavigationLink {
+                            NormalRoomView()
+                        } label: {
+                            Text("Enter Room 1")
+                                .foregroundColor(.black)
+                                .padding()
+                                .background(.white)
+                                .cornerRadius(8)
+                        }
+                        .padding()
+                        Spacer()
+                        Spacer()
+                    }
+                    Spacer()
+                    Spacer()
+                    Spacer()
+                    Spacer()
+                    Spacer()
+                    Spacer()
+                    Spacer()
+                    Spacer()
+                }
+                HStack {
+                    Spacer()
+                    Spacer()
+                    VStack {
+                        Spacer()
+                        Spacer()
+                        Spacer()
+                        Spacer()
+                        NavigationLink {
+                            BuffetAreaView()
+                        } label: {
+                            Text("Enter Room 3")
+                                .foregroundColor(.black)
+                                .padding()
+                                .background(.white)
+                                .cornerRadius(8)
+                        }
+                        .padding()
+                        Spacer()
+                        Spacer()
+                    }
+                    Spacer()
+                    Spacer()
+                    Spacer()
+                    Spacer()
+                    Spacer()
+                    Spacer()
+                    Spacer()
+                    Spacer()
+                }
+                
+                HStack {
+                    Spacer()
+                    Spacer()
+                    Spacer()
+                    Spacer()
+                    Spacer()
+                    Spacer()
+                    Spacer()
+                    Spacer()
+                    VStack {
+                        Spacer()
+                        NavigationLink {
+                            LoungeView()
+                        } label: {
+                            Text("Enter Room 2")
+                                .foregroundColor(.black)
+                                .padding()
+                                .background(.white)
+                                .cornerRadius(8)
+                        }
+                        .padding()
+                        Spacer()
+                        Spacer()
+                    }
+                    Spacer()
+                    Spacer()
+                }
+                HStack {
+                    Spacer()
+                    Spacer()
+                    Spacer()
+                    Spacer()
+                    Spacer()
+                    Spacer()
+                    Spacer()
+                    Spacer()
+                    VStack {
+                        Spacer()
+                        Spacer()
+                        NavigationLink {
+                            SuiteView()
+                        } label: {
+                            Text("Enter Room 4")
+                                .foregroundColor(.black)
+                                .padding()
+                                .background(.white)
+                                .cornerRadius(8)
+                        }
+                        .padding()
+                        Spacer()
+                    }
+                    Spacer()
+                    Spacer()
+                }
+                
+                
+                VStack {
+                    Spacer()
+                    Spacer()
+                    Spacer()
+                    Spacer()
+                    Spacer()
+                    Spacer()
+                    Spacer()
+                    Spacer()
+                    Spacer()
+
+                    HStack {
+                        Spacer()
+                        Spacer()
+                        Image(systemName: "checklist.checked.rtl")
+                        Spacer()
+                        Spacer()
+                        Spacer()
+                        Spacer()
+                        Image(systemName: "building")
+                        Spacer()
+                        Spacer()
+                        Spacer()
+                        Spacer()
+                        Image(systemName: "questionmark")
+                        Spacer()
+                        Spacer()
+                    }
+                    Spacer()
+                }
+                
+                VStack {
+                    Spacer()
+                    HStack {
+                        Spacer()
+                        NavigationLink {
+                            MCQView()
+                        } label: {
+                            Text("MCQ")
+                                .foregroundColor(.black)
+                                .padding()
+                                .background(Color(red: 255/255, green: 248/255, blue: 232/255)
+                                    .cornerRadius(8))
+                        }
+                        .padding()
+                        
+                        Spacer()
+                        NavigationLink {
+                            ContentView()
+                        } label: {
+                            Text("My Hotel")
+                                .foregroundColor(.black)
+                                .padding()
+                                .background(Color(red: 255/255, green: 248/255, blue: 232/255)
+                                    .cornerRadius(8))
+                        }
+                        .padding()
+                        Spacer()
+                        NavigationLink {
+                            LearnView()
+                        } label: {
+                            Text("Learn")
+                                .foregroundColor(.black)
+                                .padding()
+                                .background(Color(red: 255/255, green: 248/255, blue: 232/255)
+                                    .cornerRadius(8))
+                        }
+                        .padding()
+                        Spacer()
+                    }
+                                    }
+                                    .navigationTitle("My Hotel")
+                            
+                            
+                        }
+            }
         }
     }
-}
 
 #Preview {
     ContentView()
---
+++ Challenge2App/LoungeView.swift
@@ -0,0 +1,22 @@
+//
+//  LoungeView.swift
+//  Challenge2App
+//
+//  Created by Tiffany on 16/8/25.
+//
+
+import SwiftUI
+
+struct LoungeView: View {
+    var body: some View {
+        ZStack {
+            Color(red: 255/255, green: 248/255, blue: 232/255)
+                .ignoresSafeArea()
+            Text("LoungeView")
+        }
+    }
+}
+
+#Preview {
+    LoungeView()
+}
